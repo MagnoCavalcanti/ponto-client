a3871dfbd7ee3dafb089a8349fccd22b4c53902f
MagnoCavalcanti <magnocavalcanti9@gmail.com>
2025-12-15 00:57:34 -0300

    fix: corrige cores do sidebar

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
 import { useLocation, useNavigate } from "react-router";
 import { Button } from "../ui/button";
+import { Calendar, Clock4, LayoutDashboard, Users } from "lucide-react";
 
 
 const SideBar = () => {
@@ -11,41 +12,46 @@ const SideBar = () => {
     const menuItems = [
         {
         title: "Dashboard",
-        href: "/dashboard",
-        active: location.pathname === `/${empresa}/dashboard`
+        href: `/${empresa}/dashboard`,
+        active: location.pathname === `/${empresa}/dashboard`,
+        icon: <LayoutDashboard />
         },
         {
         title: "Funcionários",
-        href: "/funcionarios",
-        active: location.pathname === `/${empresa}/funcionarios` || location.pathname.startsWith(`/${empresa}/funcionarios/`)
+        href: `/${empresa}/funcionarios`,
+        active: location.pathname === `/${empresa}/funcionarios` || location.pathname.startsWith(`/${empresa}/funcionarios/`),
+        icon: <Users />
         },
         {
         title: "Registros de Ponto",
-        href: "/registros",
-        active: location.pathname === `/${empresa}/registros`
+        href: `/${empresa}/registros`,
+        active: location.pathname === `/${empresa}/registros`,
+        icon: <Calendar />
         },
         {
         title: "Dispositivos REP",
-        href: "/dispositivos",
-        active: location.pathname === `/${empresa}/dispositivos`
+        href: `/${empresa}/dispositivos`,
+        active: location.pathname === `/${empresa}/dispositivos`,
+        icon: <Clock4 />
         }
     ];
 
     return (
-        <div className={cn("pb-12 min-h-screen")}>
+        <div className="pb-12 h-full">
         <div className="space-y-4 py-4">
             <div className="px-3 py-2">
-            <div className="space-y-1">
+            <div className="space-y-1 ">
                 {menuItems.map((item, index) => (
                 <Button
                     key={index}
                     variant={item.active ? "secondary" : "ghost"}
                     className={cn(
                     "w-full justify-start",
-                    item.active && "bg-gradient-primary text-primary-foreground hover:bg-gradient-primary"
+                    item.active && "bg-[#171717] text-white hover:bg-[#171717]"
                     )}
                     onClick={() => navigate(item.href)}
                 >
+                    {item.icon && <span className="mr-2">{item.icon}</span>}
                     {item.title}
                 </Button>
                 ))}
